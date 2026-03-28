efb1bd6a2a0aa87009ffee4e78dc1b958b24b7f4
RAVI <raviqs@yahoo.com>
2026-03-28 11:56:13 +0530

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -8,13 +8,13 @@ import { getFirestore } from 'firebase/firestore'
 import { getStorage } from 'firebase/storage'
 
 const firebaseConfig = {
-  apiKey:            import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain:        import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId:         import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket:     import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId:             import.meta.env.VITE_FIREBASE_APP_ID,
-}
+  apiKey: "AIzaSyBdfo2m9fdCNvx8e0pPgX3H-UD7b90QwH0",
+  authDomain: "listeninghub-50d02.firebaseapp.com",
+  projectId: "listeninghub-50d02",
+  storageBucket: "listeninghub-50d02.firebasestorage.app",
+  messagingSenderId: "240024230551",
+  appId: "1:240024230551:web:e8ba492834e1a3fcd7a7d2"
+};
 
 const app = initializeApp(firebaseConfig)
 
